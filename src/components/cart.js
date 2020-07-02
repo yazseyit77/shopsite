add83b24f3514ec6835f656e7328fdc867c8b862
yazseyit77 <yazgeldi.seyidov@gmail.com>
2020-07-02 14:03:18 -0700

    cart total price value functioned

--- src/components/cart.js
+++ src/components/cart.js
@@ -3,11 +3,12 @@ import { CartContext } from "./cartContext";
 
 const Cart = (props) => {
   const [cart, setCart] = useContext(CartContext);
+  const totalPrice = cart.reduce((a, b) => b.price + a, 0);
   return (
     <div>
       <h2>Cart Items: {cart.length}</h2>
       <br />
-      <h4>Total: 0</h4>
+      <h4>Total: {totalPrice}</h4>
       <br />
       <hr />
     </div>
